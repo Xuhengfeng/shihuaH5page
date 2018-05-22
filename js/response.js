f5fec61330448b207db5f3b0d7268db062da09bb
xuhengfeng <564297479@qq.com>
2018-05-22 09:30:04 +0800

    update

---
+++ js/response.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: 徐横峰 
+ * @Date: 2018-05-22 09:28:35 
+ * @Last Modified by: 564297479@qq.com
+ * @Last Modified time: 2018-05-22 09:29:27
+ */
+window.onload=window.onresize=window.onpageshow=function() {
+    var wWidth = window.innerWidth;
+    if(wWidth>=414) wWidth = 414;
+    document.getElementsByTagName("html")[0].style.fontSize = wWidth/750 * 100 + 'px';
+}
