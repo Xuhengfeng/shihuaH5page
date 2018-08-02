93b373dd3d0ac3614ce2d084014f4393ec7e1cb4
xuhengfeng <564297479@qq.com>
2018-08-02 18:22:40 +0800

    修复bug

--- calc/js/calc.js
+++ calc/js/calc.js
@@ -2,32 +2,29 @@ var vm = new Vue({
     el: '#box',
     data:{
         menu:['公积金贷款','商业贷款','组合贷款'],
-        // input的双向绑定
         mode: "按贷款总额",//计算模式
         isLoans: true,//按贷款计算方式
-        loans: 0,//贷款总额
+        loans: null,//贷款总额
+        loans2: null,//贷款总额上限
+        sellPrice: null,//房价总额
+        sellPrice2: null,//房价总额上限
+        houseLoans: null,//组合贷款的公积金贷款
+        commercialLoans: null,//组合贷款的商业贷款
         years: "30年",//贷款年限
         rate11: "最新基准利率(3.25%)",//公积金贷款利率
         rate21: "最新基准利率(4.9%)",//商业贷款利率
-        rate31: "最新基准利率(3.25%)",//组合贷款的公积金贷款利率
-        rate32: "最新基准利率(4.9%)",//组合贷款的商业贷款利率
         calcRate1: 3.25, //用于计算的公积金贷款利率
         calcRate2: 4.9,  //用于计算的商业贷款利率
         calcYears: 30,   //用于计算的贷款年数
-        commercialLoans: 0,//组合贷款的商业贷款
-        houseLoans: 0,//组合贷款的公积金贷款
         isToast: false,//是否提示
         toastContent: "请输入贷款金额",//提示内容
         rate: "7成",//公积金 商业 组合的贷款比例
 
-        sellPrice: 0,//房价总额
-
         selectNum: 0,//tab切换的索引
-        selectNum3: 3,//用于区分组合贷款中的3公积金贷款比例 4商业贷款比例
-        one: null,
-        two: null,
-        three: null,
-        four: null,
+        selectNum1: null,
+        selectNum2: null,
+        selectNum3: null,
+        selectNum4: null,
 
         isCancel: false,//打开展开选项
         ergodic : null,//展开项将要的遍历对象
@@ -44,55 +41,54 @@ var vm = new Vue({
         this.selectItem(1);
     },
     watch: {
+        // 房价
+        sellPrice(){
+            this.sellPrice2 = this.sellPrice;
+            if(this.mode=='按房价总额'){
+                this.loans = parseInt(this.sellPrice*7/10);
+                this.loans2 = this.loans;
+            }
+        },
+        // 贷款金额
         loans() {
-            this.commercialLoans = this.loans - 0;
-        },
-        commercialLoans() {
-            if(this.commercialLoans<0) this.commercialLoans=0;
-            if(this.loans==0){
-                return;
+            if(this.mode=='按房价总额'){
+                if(this.sellPrice2!=null){
+                    if(this.loans>this.loans2) {
+                        this.isToast=true;
+                        this.toastContent="超出贷款总额";
+                        setTimeout(()=>{this.isToast=false},3000);
+                    }
+                }
             }else{
-                this.commercialLoans>this.loans
-                ? (this.isToast=true,this.toastContent="超出贷款总额",setTimeout(()=>{this.isToast=false},3000))
-                : this.houseLoans = this.loans - this.commercialLoans;
+                this.commercialLoans = this.loans;
             }
         },
+        // 贷款比例
+        rate() {
+            if(this.mode=='按房价总额'){
+                this.loans = parseInt(parseInt(this.rate)/10*this.sellPrice);
+                this.loans2 = this.loans;
+            }
+        },
+        // 公积金贷款
         houseLoans() {
-            if(this.houseLoans<0) this.houseLoans=0;
-            if(this.loans==0){
-                return;
-            }else{
-                this.houseLoans>this.loans
-                ? (this.isToast=true,this.toastContent="超出贷款总额",setTimeout(()=>{this.isToast=false},3000))
-                : this.commercialLoans = this.loans - this.houseLoans;
+            if((this.commercialLoans+this.houseLoans)>this.loans){
+                this.isToast=true;
+                this.toastContent="超出贷款总额";
+                setTimeout(()=>{this.isToast=false},3000);
             }
-        }
-    },
-    computed: {
-        //修正高亮样式
-        selectNum2() {
-            let num = this.titleIndex;
-            switch(num) {
-                case 0: return this.one;
-                case 1: return this.two;
-                case 2: return this.three;
-                case 3: return this.four;
-            }
+            this.commercialLoans = this.loans - this.houseLoans;
         }
     },
     methods:{
-        aaa() {
-           this.loans = '';
-           this.loans = '0';
-        },
         /**
          * selectItem(index) 
          * index  0 1 2 控制菜单对应高亮
          * index 3 4 控制贷款利率 公积金利率 商贷利率
          */
         changeLoans() {
-            this.commercialLoans = this.loans - this.houseLoans;
             this.houseLoans = this.loans - this.commercialLoans;
+            this.commercialLoans = this.loans - this.houseLoans;
         },
         selectItem(index) {
             let num;
@@ -119,18 +115,22 @@ var vm = new Vue({
             switch(num){
                 case 0: 
                     this.titleIndex = 0;
+                    this.sellPrice2 = null;
                     this.ergodic  = this.calcWay;//计算方式
                     break;
                 case 1: 
                     this.titleIndex = 1;
+                    this.sellPrice2 = null;                    
                     this.ergodic  = this.loanYears;//贷款年限                     
                     break;
                 case 2: 
                     this.titleIndex = 2;
+                    this.sellPrice2 = null;                    
                     this.ergodic  = this.loanRate;//贷款利率                                                       
                     break;
                 case 3:
                     this.titleIndex = 3;
+                    this.sellPrice2 = null;                    
                     this.ergodic  = this.loanRate2;//贷款比例                                                     
                     break;
             }
@@ -142,7 +142,7 @@ var vm = new Vue({
             switch(num){
                 case 0://计算方式
                     this.mode = item;
-                    this.one = index;
+                    this.selectNum1 = index;
                     if(item=="按房价总额"){
                         this.isLoans=false;
                     }else if(item == "按贷款总额") {
@@ -152,12 +152,10 @@ var vm = new Vue({
                 case 1://年限
                     this.years=item+'年';
                     this.calcYears=item;
-                    this.two = index;
+                    this.selectNum2 = index;
                     if(item<=5&&item>1){
                         this.rate11="最新基准利率(2.75%)";
                         this.rate21="最新基准率(4.75%)";
-                        this.rate31="最新基准利率(2.75%)";
-                        this.rate32="最新基准率(4.75%)";
                         if(this.selectNum==0){
                             this.calcRate=2.75;
                         }else if(this.selectNum==1){
@@ -167,14 +165,10 @@ var vm = new Vue({
                     else if(item == 1) {
                         this.rate11="最新基准利率(2.75%)";
                         this.rate21="最新基准率(4.35%)";
-                        this.rate31="最新基准利率(2.75%)";
-                        this.rate32="最新基准率(4.35%)";
                     }
                     else{
                         this.rate11="最新基准利率(3.25%)";
                         this.rate21="最新基准率(4.9%)";
-                        this.rate31="最新基准利率(3.25%)";
-                        this.rate32="最新基准率(4.9%)";
                         if(this.selectNum==0){
                             this.calcRate=3.25;
                         }else if(this.selectNum==1){
@@ -183,97 +177,69 @@ var vm = new Vue({
                     }
                     break;
                 case 2://利率
-                    this.three = index;
+                    this.selectNum3 = index;
                     this.changeRate(item,index,this.selectNum);
                     break;
                 case 3://贷款比例
-                    this.four = index;
+                    this.selectNum4 = index;
                     this.rate=item+'成';
                     break;
             }
             this.cancel();
         },
-        changeRate(item,index,selectNum) {
-            let resultRate;
-            let num = item.slice(0,-1)-0;
-            if(this.selectNum == 0){
-                //修改贷款利率 基准利率3.25%
-                resultRate = Math.floor( num * 3.25 * 100) / 100//不四舍五入 截取小数点后两位 思想是先扩大100倍取整，再缩小100倍
-                this.calcRate1 = resultRate;
-
-                index==0
-                ? this.rate11 = "最新基准利率(3.25%)"
-                : this.rate11 = "最新基准利率"+item+"("+resultRate+"%)";
-            }else if(this.selectNum == 1) {
-                //修改贷款利率 基准利率4.9%
-                if(item.slice(-1)=='折'){
-                    resultRate = num * 4.9 / 10;
-                }else if(item.slice(-1)=='倍'){
-                    resultRate = num * 4.9;                                   
-                }
-                resultRate = Math.floor(resultRate * 100) / 100//不四舍五入 截取小数点后两位 思想是先扩大100倍取整，再缩小100倍
-                this.calcRate2 = resultRate;
+        changeRate(item,index) {
+            var resultRate1;//公积金贷款利率
+            var resultRate2;//商业贷款利率
+            // 取出数字
+            var num = item.slice(0,-1)-0;
                 
-                index==0
-                ? this.rate21 = "最新基准利率(4.9%)"
-                : this.rate21 = "最新基准利率"+item+"("+resultRate+"%)";
-            }else if(this.selectNum == 2){
-                if( this.selectNum3 == 3) {
-                    //修改贷款利率 基准利率3.25%
-                    resultRate = Math.floor(num * 3.25 * 100) / 100//不四舍五入 截取小数点后两位 思想是先扩大100倍取整，再缩小100倍
-                    this.calcRate1 = resultRate;
-
-                    index==0
-                    ? this.rate31 = "最新基准利率(3.25%)"
-                    : this.rate31 = "最新基准利率"+item+"("+resultRate+"%)";
-                }else{
-                    //修改贷款利率 基准利率4.9%
-                    if(item.slice(-1)=='折'){
-                        resultRate = num * 4.9 / 10;
-                    }else if(item.slice(-1)=='倍'){
-                        resultRate = num * 4.9;                                   
-                    }
-                    resultRate = Math.floor(resultRate * 100) / 100//不四舍五入 截取小数点后两位 思想是先扩大100倍取整，再缩小100倍
-                    this.calcRate2 = resultRate;
-                    
-                    index==0
-                    ? this.rate32 = "最新基准利率(4.9%)"
-                    : this.rate32 = "最新基准利率"+item+"("+resultRate+"%)";
-                }
+            //修改利率 基准利率3.25%
+            resultRate1 = Math.floor( num * 3.25 * 100) / 100//不四舍五入 截取小数点后两位 思想是先扩大100倍取整，再缩小100倍
+            this.calcRate1 = resultRate1;
+            
+            //修改利率 基准利率4.9%
+            if(item.slice(-1)=='折'){
+                resultRate2 = num * 4.9 / 10;
+            }else if(item.slice(-1)=='倍'){
+                resultRate2 = num * 4.9;                                   
             }
+            resultRate2 = Math.floor(resultRate2 * 100) / 100//不四舍五入 截取小数点后两位 思想是先扩大100倍取整，再缩小100倍
+            this.calcRate2 = resultRate2;
+            
+            //公积金贷款利率显示
+            index==0
+            ? this.rate11 = "最新基准利率(3.25%)"
+            : this.rate11 = "最新基准利率"+item+"("+resultRate1+"%)";
+
+            //商业贷款利率显示
+            index==0
+            ? this.rate21 = "最新基准利率(4.9%)"
+            : this.rate21 = "最新基准利率"+item+"("+resultRate2+"%)";
         },
         //计算
         calcResult() {
             //验证是否输入贷款额 
-            if(this.loans==0){
+            if(this.loans==null){
                 this.isToast = true;
                 return setTimeout(()=>{this.isToast=false},3000);
+            }else if(this.mode=='按房价总额'){
+                if(this.loans>this.sellPrice){
+                    this.isToast=true;
+                    this.toastContent="超出贷款总额";
+                    setTimeout(()=>{this.isToast=false},3000);
+                    return;
+                }
             }
-
             //这里调取真正的计算
             let result; 
             if(this.selectNum == 0){//公积金贷款按贷款总额计算
-                if(this.isLoans) {
-                    result = this.calcformula1(0);
-                }else{
-                    result = this.calcformula2(0);
-                }
+                result = this.calcformula(0);
             }else if(this.selectNum == 1){//商业贷款按贷款总额计算
-                if(this.isLoans) {
-                    result = this.calcformula1(1);
-                }else{
-                    result = this.calcformula2(1);
-                }
-            
+                result = this.calcformula(1);
             }else{//组合贷款按贷款总额计算
-                if(this.isLoans){
-                    result = this.groupCalc();
-                }else{
-                    result = this.groupCalc();
-                }
+                result = this.groupCalc();
             }
-
-            if(this.loans!=0) location.href="./calcChart.html?one="+JSON.stringify(result.one)+"&two="+JSON.stringify(result.two);
+            if(this.loans!=null) location.href="./calcChart.html?selectNum1="+JSON.stringify(result.selectNum1)+"&selectNum2="+JSON.stringify(result.selectNum2);
         },
         /**
          * 计算公式
@@ -310,7 +276,7 @@ var vm = new Vue({
          * 支付利息 = 月均还款*还款月数 - 贷款金额
          */
         //公积金和商业贷款
-        calcformula1(num) {
+        calcformula(num) {
             //按照贷款总额进行计算
             let rate = num == 0 ? this.calcRate1 : this.calcRate2;
             let D = (this.loans-0)*10000;    //贷款总额多少元
@@ -360,8 +326,8 @@ var vm = new Vue({
             result2.totalInterest2 = Math.floor((rnb*month-D)*10)/10;
             result2.totalRnb = result2.totalInterest + result2.totalLoans2;
             return {
-                one: result1,
-                two: result2
+                selectNum1: result1,
+                selectNum2: result2
             };
         },
         //组合贷款计算
@@ -420,65 +386,8 @@ var vm = new Vue({
             result2.totalRnb = result2.totalInterest + result2.totalLoans2;
 
             return {
-                one: result1,
-                two: result2
-            };
-        },
-        //按房价总额进行计算
-        calcformula2(num) {
-            console.log('点击')
-            //按照房价总额进行计算
-            let rate = num == 0 ? this.calcRate1 : this.calcRate2;
-            let housesell = this.sellPrice;  //房价总额
-            let D = (this.loans-0)*10000;    //贷款总额多少元
-            let R = rate/100;                //贷款利率(年利率)
-            let r = R/12;                    //月利率 
-            let years = this.calcYears;      //贷款年数
-            let month = years*12;            //月数
-
-            let result1 = {                //等额本金
-                totalLoans: D,             //贷款总额多少元
-                totalLoans2: D/10000,      //贷款总额多少万
-                month: month,              //月数
-                rnb: null,                 //每月还款多少元
-                totalRnb: null,            //总共还多少元
-                totalInterest: null,       //总共支付多少万利息
-                totalInterest2: null,      //总共支付多少元利息
-                Decreasing: null,          //每月递减多少元
-            }
-            let result2 = {                //等额本息
-                totalLoans: D,             //贷款总额多少元
-                totalLoans2: D/10000,      //贷款总额多少万
-                month: month,              //月数
-                rnb: null,                 //每月还款多少元
-                totalRnb: null,            //总共还多少元
-                totalInterest: null,       //总共支付多少万利息
-                totalInterest2: null,      //总共支付多少元利息
-            }
-
-            //年数
-            "string" == typeof this.years
-            ? years = this.years.slice(0,-1)-0
-            : years = this.years;
-
-            //等额本金总利息
-            let totalInterest = [(D/month+D*r)+D/month*(1+r)]/2*month-D;
-            result1.totalInterest = Math.floor(totalInterest/10000*10)/10;
-            result1.totalInterest2 = Math.floor(totalInterest);
-            result1.totalRnb = result1.totalInterest + result1.totalLoans2;
-            result1.rnb = Math.floor((D - result1.totalRnb)*r + D/month);
-            result1.Decreasing = Math.floor((D/month*r));
-
-            //等额本息月均还款
-            let midn = Math.pow((1+r), month);
-            let rnb = (D*r*midn)/(midn-1);
-            result2.rnb = Math.floor(rnb);
-            result2.totalInterest = Math.floor((rnb*month-D)/10000*10)/10;
-            result2.totalInterest2 = Math.floor((rnb*month-D)*10)/10;
-            result2.totalRnb = result2.totalInterest + result2.totalLoans2;
-            return {
-                one: result1,
-                two: result2
+                selectNum1: result1,
+                selectNum2: result2
             };
         }
     }
