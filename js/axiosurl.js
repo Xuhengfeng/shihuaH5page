763f03c29e3e81ade07c50aaf21ffee00bc76ced
Aming <zhaotanming@zhuotaiju.com>
2018-09-05 16:07:06 +0800

    积分卡劵相关页面

--- js/axiosurl.js
+++ js/axiosurl.js
@@ -23,7 +23,17 @@ let CONSULTANTINFO = SERVER_IP + '/consultant/info'//获取顾问详情
 let PROBLEMINFO = SERVER_IP + '/consultant/problem_info'//获取问题详情
 let TWOHOUSELIST = SERVER_IP + '/house/query'//二手房列表
 let RENTHOUSELIST = SERVER_IP + '/rentHouse/query'//租房列表
+let CODEKEY = "29e94f94-8664-48f2-a4ff-7a5807e13b68"//验证码
+let ADDCODE =  SERVER_IP+'/member/fetchSmsCode'//获取验证码接口
+
+let MYDATA = SERVER_IP+"/member/myinfo";//个人信息获取接口
 
 let AGREET = SERVER_IP + '/consultant/adopt'//采纳
 let LOOK = SERVER_IP + '/consultant/concern'//是否关注
+//积分
+let SCOREGIFT = SERVER_IP+'/score/queryGoodsList'//查询积分可兑换商品列表
+let GETGOODSDETAIL = SERVER_IP+'/score/getGoodsDetail'//查看商品的详细信息\
+let EXCHANGE = SERVER_IP + '/score/exchange'//积分兑换礼品
+let GETCOUPONDETAIL = SERVER_IP + '/coupon/getCouponDetail'//已兑换的卡劵详情
+let GETGIVECOUPONDETAIL = SERVER_IP + '/coupon/getGiveCouponDetail'//已转赠卡劵详情
 
